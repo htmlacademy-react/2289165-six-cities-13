bac279e696d131cccde06ad13e122bd4a0368509
Semenets Ekaterina <cortes.lex@gmail.com>
2023-07-14 12:11:50 +0300

    Добавлена маршрутизация к страницам сайта

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,48 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavouritesPage from '../../pages/favourites-page/favourites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 
 type AppProps = {
   placesCount: number;
 }
 
-function App({placesCount}: AppProps): JSX.Element {
+function App({ placesCount }: AppProps): JSX.Element {
   return (
-    <MainPage placesCount = {placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          path={AppRoute.MainPage}
+          element={<MainPage placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.LoginPage}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.FavouritesPage}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavouritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.OfferPage}
+          element={<OfferPage />}
+        />
+        <Route
+          path={AppRoute.PageNotFound}
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -2,4 +2,18 @@ const Setting = {
   PlacesCount: 100
 };
 
-export {Setting};
+const AppRoute = {
+  MainPage: '/',
+  LoginPage: '/login',
+  FavouritesPage: '/favorites',
+  OfferPage: '/offer/:id',
+  PageNotFound: '*'
+} as const;
+
+enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+export {Setting, AppRoute, AuthorizationStatus};
